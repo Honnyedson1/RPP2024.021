63f6eb0e813d6429cbe6a0bb70d3a91f114d93f3
MayraRegina <Mayra.r@escolar.ifrn.edu.br>
2024-11-29 05:59:36 -0300

    ...,

--- Lights of darkness/Assets/Honnyedson/Scripts/UI/MainMenuManager.cs
+++ Lights of darkness/Assets/Honnyedson/Scripts/UI/MainMenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject Options;
+    public GameObject PauseImage;
     public void PlayGame()
     {
         SceneManager.LoadScene("Vilarejo");
     }
+    public void OptionsGame()
+    {
+        Options.gameObject.SetActive(true);
+        PauseImage.gameObject.SetActive(false);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Voltar()
+    {
+        Options.gameObject.SetActive(false);
+        PauseImage.gameObject.SetActive(true);
+    }
+    
 }
